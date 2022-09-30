ecd63805501a7d6028984176b75f88a870cb6af2
Viacheslav Grachev <slavamgtu@yandex.ru>
2022-09-30 11:35:37 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+""" задача создать гитхаб репозиторий где будет ветвление проекта
+и действия все по изменению в проекте проводить в PyCharm """
+
+number_ver_prodj=1
+name_prodj="Git in PyCharm"
+print(f'Your project is "{name_prodj}.{number_ver_prodj}"')
