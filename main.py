e71f396c43aa098edcbf737a072447cc7f969fe0
Viacheslav Grachev <slavamgtu@yandex.ru>
2022-10-01 13:06:14 +0400

    made change 1 to the local file and и Reformat code

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 """ задача создать гитхаб репозиторий где будет ветвление проекта
 и действия все по изменению в проекте проводить в PyCharm """
 
-number_ver_prodj=1
-name_prodj="Git in PyCharm"
+number_ver_prodj = 1
+name_prodj = "Git in PyCharm"
+'made change 1 to the local file and и Reformat code'
+number_ver_prodj += 1
 print(f'Your project is "{name_prodj}.{number_ver_prodj}"')
