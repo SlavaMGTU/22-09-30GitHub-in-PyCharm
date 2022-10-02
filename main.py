a67e2c42462b108e10aeb9b39c37c01651509553
Viacheslav Grachev <slavamgtu@yandex.ru>
2022-10-02 17:18:53 +0400

    'made change 4 to the local file and и Reformat code+ return to develop branch'

--- main.py
+++ main.py
@@ -7,4 +7,5 @@ name_prodj = "Git in PyCharm"
 number_ver_prodj += 1
 'made change 2 to the local file and и Reformat code+ new branch'
 number_ver_prodj += 0.1
+'made change 4 to the local file and и Reformat code+ return to develop branch'
 print(f'Your project is "{name_prodj}.{number_ver_prodj}"')
